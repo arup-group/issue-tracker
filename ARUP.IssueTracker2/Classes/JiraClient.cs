a73b45eb9107af37205034f1a19ee3b703d1d817
Huan Ting Chen <huan-ting.chen@arup.com>
2019-07-15 14:27:12 +0100

    enforce TSL 1.2

--- ARUP.IssueTracker2/Classes/JiraClient.cs
+++ ARUP.IssueTracker2/Classes/JiraClient.cs
@@ -4,11 +4,16 @@ using System.Linq;
 using System.Text;
 using ARUP.IssueTracker.UserControls;
 using Arup.RestSharp;
+using System.Net;
 
 namespace ARUP.IssueTracker.Classes
 {
     public static class JiraClient
     {
+        static JiraClient()
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+        }
         private static RestClient client;
         public static RestClient Client
         {
